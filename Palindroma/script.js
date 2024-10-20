e74ec108edeb418df764ee4ac034c33c85136a28
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-20 12:03:39 +0200

    work complete

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -2,12 +2,27 @@
 //* Palidroma
 
 //# TRACCIA
-
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
 //# RACCOLTA DATI
+const parolaUtente = prompt(
+  "Scrivi la parola che vuoi verificare sia palindroma: "
+);
 
 //# ELABORAZIONE
+function checkPalindromy(parola) {
+  if (parola.length > 0) {
+    for (let i = 0; i < parola.length / 2; i++) {
+      if (parola[i] !== parola[parola.length - i - 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const palindroma = checkPalindromy(parolaUtente);
+console.log(palindroma);
 
 //# OUTPUT
--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -10,7 +10,39 @@
 // Dichiariamo chi ha vinto.
 
 //# RACCOLTA DATI
+let PariODispUtente = prompt("Scegli pari o dispari: ");
+PariODispUtente = PariODispUtente.toLowerCase();
+const numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5: "));
+//* let numeroValido = false;
+//* if (numeroUtente >= 1 && numeroUtente <= 5) {
+//*   numeroValido = true;
+//* }
+//* while (!numeroValido) {
+//*   numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5: "));
+//*   if (numeroUtente >= 1 && numeroUtente <= 5) {
+//*     numeroValido = true;
+//*   }
+//* }
 
 //# ELABORAZIONE
+function generaNumeroCpu(min, max) {
+  return parseInt(Math.random() * max) + min;
+}
+
+const sum = numeroUtente + generaNumeroCpu(1, 5);
+console.log(sum);
+function verificaParioDisp(numero) {
+  if (numero % 2 === 0) {
+    return "pari";
+  } else {
+    return "dispari";
+  }
+}
 
 //# OUTPUT
+
+if (verificaParioDisp(sum) === PariODispUtente) {
+  alert("Ha vinto il giocatore");
+} else {
+  alert("Ha vinto la cpu");
+}
